94505e6bb91822842c2c6963754145a3db411839
Moiz Ahmed <moeezinbox@gmail.com>
2025-10-22 12:34:56 +0530

    Hamburger Menu - WPF Custom User Control.

---
+++ HamburgerMenu/HamburgerMenuControl/HamburgerMenuItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HamburgerMenuControl;
+
+public class HamburgerMenuItem : RadioButton
+    {
+        static HamburgerMenuItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HamburgerMenuItem), new FrameworkPropertyMetadata(typeof(HamburgerMenuItem)));
+        }
+    }
