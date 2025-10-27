3780d125163a3252986412a1d04d1ab9c62c5704
Moiz Ahmed <moeezinbox@gmail.com>
2025-10-27 12:43:48 +0530

    Tooltip in WPF application.

---
+++ AnimatedTooltip/TooltipPopup/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace TooltipPopup;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Home_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Home;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Home";
+        }
+
+        private void Home_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Profile_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Profile;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Profile";
+        }
+
+        private void Profile_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Settings_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Settings;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Settings";
+        }
+
+        private void Settings_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+}
---
+++ AnimatedTooltip/TooltipPopup/PopupUserControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace TooltipPopup
+{
+    /// <summary>
+    /// Interaction logic for PopupUserControl.xaml
+    /// </summary>
+    public partial class PopupUserControl : UserControl
+    {
+        public PopupUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
