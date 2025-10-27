d1a643e9edcabc9af9c3b5c27531abf3c6c99876
Moiz Ahmed <moeezinbox@gmail.com>
2025-10-27 13:21:33 +0530

    Responsive UI design in WPF application.

---
+++ ResponsiveUI/ResponsiveApp/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ResponsiveApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
